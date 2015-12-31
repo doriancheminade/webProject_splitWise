4a88e428c3b7db60253f196ed4a8f09cf6bd8d46
doriancheminade <cheminadedorian@gmail.com>
2015-12-31 20:27:07 +0100

    started to do the total balance

--- app.js
+++ app.js
@@ -15,6 +15,45 @@ MongoClient.connect(url, function(err, db) {
 		    resp.json(data)
 		})
     })
+    app.get("/api/bill/total/", function(req, resp){
+        var u = req.query.user;
+        /*        
+        var b = bills.aggregate([
+            {$match: {$or: [{payed_by: u}, {split_with: {$elemMatch: {name: u}}}]}},
+            {$group:{
+                    payed:{$sum: {$cond: {if:{payed_by: u}, then: 'price', else: 0}}},
+                    debt:{$sum: {$cond: {if:{split_with: {$elemMatch: {name: u}}}, then: split_with.u, else: 0}}}                    
+                    }
+             }
+        ]);
+        */      
+        var p = bills.aggregate([
+            {$match: {payed_by: u}},
+            {$group:{
+                _id:"$payed_by",
+                count:{$sum: 1},
+                payed: {$sum: "$price"}           
+            }}
+        ]);
+        var d = bills.aggregate([
+            {$match: {split_with: {$elemMatch: {name: u}}}},
+            {$unwind: "$split_with"},
+            {$group: {
+                _id: "$split_with.name",
+                count:{$sum: 1},
+                debt:{$sum: "$split_with.owe"}
+            }}
+        ]);
+        
+        p.toArray(function(err, d1){
+		    if (err) return resp.json(err)
+		        d.toArray(function(err, d2){
+		        if (err) return resp.json(err)
+		        resp.json([d1[0],d2[0]]);
+		    })
+		})  
+		
+    })
 
     app.listen(port, function() {
         console.log("Server running on port "+port)
